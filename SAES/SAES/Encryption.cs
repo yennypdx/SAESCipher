d83ccf6749623663ec6a4222af69356803b34906
ypurwanto <yenny.purwanto@oit.edu>
2019-08-15 12:19:27 -0700

    completed LibraryAES for encryption and Encryption classes

--- SAES/SAES/Encryption.cs
+++ SAES/SAES/Encryption.cs
@@ -8,10 +8,82 @@ namespace SAES
 {
     public class Encryption
     {
-        LibrarySAES lib = new LibrarySAES();
+        private static readonly LibrarySAES lib = new LibrarySAES();
         public Encryption() { }
 
-        // TODO: ENCRYPTION SEQUENCE
+        /* Step 1: Convert numerical to binary */
+        List<int> originalMsg = new List<int>();
+        List<int> originalKey = new List<int>();
+        string ConvertMessageToBinaryClick(string message)
+        {
+            originalMsg = lib.ConvertStringOfNumbersToBinary(message);
+            return BuildString(originalMsg);
+        }
 
+        string ConvertKeyToBinary(string key)
+        {
+            originalKey = lib.ConvertStringOfNumbersToBinary(key);
+            return BuildString(originalKey);
+        }
+
+        /* Step 2: Permutation */
+        List<int> step2PermutateResult = new List<int>();
+        string PermutateKey()
+        {
+            step2PermutateResult = lib.PermutationOne(originalKey);
+            return BuildString(step2PermutateResult);
+        }
+
+        /* Step 3: XOR the original message with the key */
+        List<int> step3XorResult = new List<int>();
+        string XorEvaluation()
+        {
+            step3XorResult = lib.ExorEvaluation(originalMsg, originalKey);
+            return BuildString(step3XorResult);
+        }
+
+        List<int> step4SubsResult = new List<int>();
+        List<int> step7XorResult = new List<int>();
+        List<int> step7XorResultCarriedOn = new List<int>();
+        int loopCounter = 0;
+        void LoopOneTime()
+        {
+            /* Step 4: Substitute bytes, shift-1-right */
+            if (loopCounter == 1)
+                step4SubsResult = lib.ShiftOneRight(step3XorResult);
+            else
+                step4SubsResult = lib.ShiftOneRight(step7XorResultCarriedOn);
+
+            string step4OutString = BuildString(step4SubsResult);
+
+            /* Step 5: Shift rows */
+            List<int> step5ShiftResult = lib.ShiftRowsSingleRound(step4SubsResult);
+            string step5OutString = BuildString(step5ShiftResult);
+
+            /* Step 6: Shift columns */
+            List<int> step6ShiftResult = lib.MixColumnVerticallyOneRound(step5ShiftResult);
+            string step6OutString = BuildString(step6ShiftResult);
+
+            /* Step 7: XOR Step6Result with PermutatedKey */
+            step7XorResult = lib.ExorEvaluation(step6ShiftResult, step2PermutateResult);
+            step7XorResultCarriedOn = step7XorResult;
+
+            /* Step 8: Looping when prompt by user */
+            loopCounter++;
+        }
+
+        string ShowEncryptedMessage()
+        {
+            return BuildString(step7XorResult);
+        }
+
+        string BuildString(List<int> inputList)
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (var x in inputList)
+                builder.Append(x);
+
+            return builder.ToString();
+        }
     }
 }
--- SAES/SAES/LibrarySAES.cs
+++ SAES/SAES/LibrarySAES.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SAES
 {
@@ -18,14 +14,19 @@ namespace SAES
             {
                 int val = (int)Char.GetNumericValue(inputText[i]);
                 string binary = Convert.ToString(val, 2);
-                if (binary.Length != 4) {
+                if (binary.Length != 4)
+                {
                     string newBinary = binary.PadLeft(4, '0');
-                    foreach (var s in newBinary) {
+                    foreach (var s in newBinary)
+                    {
                         int temp = (int)Char.GetNumericValue(s);
                         output.Add(temp);
                     }
-                } else {
-                    foreach (var s in binary) {
+                }
+                else
+                {
+                    foreach (var s in binary)
+                    {
                         int temp = (int)Char.GetNumericValue(s);
                         output.Add(temp);
                     }
@@ -77,8 +78,9 @@ namespace SAES
         public List<int> ShiftOneRight(List<int> inputList)
         {
             List<int> output = new List<int>();
-            output.Add(inputList[15]);      
-            for (int i = 1; i < inputList.Count; i++) {   
+            output.Add(inputList[15]);
+            for (int i = 1; i < inputList.Count; i++)
+            {
                 output.Add(inputList[i - 1]);
             }
             return output;
@@ -92,7 +94,7 @@ namespace SAES
             List<int> row2 = new List<int>();
             List<int> row3 = new List<int>();
 
-            for(int i = 0; i < inputList.Count; i++)
+            for (int i = 0; i < inputList.Count; i++)
             {
                 if (i < 4)
                     row0.Add(inputList[i]);
@@ -105,12 +107,12 @@ namespace SAES
             }
 
             output = row0;
-            ShiftOneLeftRowOne(row1);            
-            output.AddRange(row1);
-            ShiftTwoLeftRowTwo(row2);
-            output.AddRange(row2);
-            ShiftThreeLeftRowThree(row3);
-            output.AddRange(row3);
+            List<int> shiftedRow1 = ShiftOneLeftRowOne(row1);
+            output.AddRange(shiftedRow1);
+            List<int> shiftedRow2 = ShiftTwoLeftRowTwo(row2);
+            output.AddRange(shiftedRow2);
+            List<int> shiftedRow3 = ShiftThreeLeftRowThree(row3);
+            output.AddRange(shiftedRow3);
 
             return output;
         }
@@ -118,7 +120,8 @@ namespace SAES
         public List<int> ShiftOneLeftRowOne(List<int> inputList)
         {
             List<int> output = new List<int>();
-            for (int i = 0; i < inputList.Count; i++) {
+            for (int i = 0; i < inputList.Count; i++)
+            {
                 if (i == 3)
                     output.Add(inputList[0]);
                 else
@@ -130,7 +133,8 @@ namespace SAES
         public List<int> ShiftTwoLeftRowTwo(List<int> inputList)
         {
             List<int> output = new List<int>();
-            for (int i = 0; i < inputList.Count; i++) {
+            for (int i = 0; i < inputList.Count; i++)
+            {
                 if (i < 2)
                     output.Add(inputList[i + 2]);
                 else
@@ -142,7 +146,8 @@ namespace SAES
         public List<int> ShiftThreeLeftRowThree(List<int> inputList)
         {
             List<int> output = new List<int>();
-            for (int i = 0; i < inputList.Count; i++) {
+            for (int i = 0; i < inputList.Count; i++)
+            {
                 if (i < 1)
                     output.Add(inputList[i + 3]);
                 else
@@ -151,5 +156,144 @@ namespace SAES
             return output;
         }
 
+        public List<int> MixColumnVerticallyOneRound(List<int> inputList)
+        {
+            List<int> output = new List<int>();
+
+            // feed in to the columns
+            List<int> col0 = new List<int>();
+            List<int> col1 = new List<int>();
+            List<int> col2 = new List<int>();
+            List<int> col3 = new List<int>();
+            for (int i = 0; i < inputList.Count; i++)
+            {
+                if (i == 0)
+                    col0.Add(inputList[i]);
+                if (i == 1)
+                    col1.Add(inputList[i]);
+                if (i == 2)
+                    col2.Add(inputList[i]);
+                if (i == 3)
+                    col3.Add(inputList[i]);
+                if (i == 4)
+                    col0.Add(inputList[i]);
+                if (i == 5)
+                    col1.Add(inputList[i]);
+                if (i == 6)
+                    col2.Add(inputList[i]);
+                if (i == 7)
+                    col3.Add(inputList[i]);
+                if (i == 8)
+                    col0.Add(inputList[i]);
+                if (i == 9)
+                    col1.Add(inputList[i]);
+                if (i == 10)
+                    col2.Add(inputList[i]);
+                if (i == 11)
+                    col3.Add(inputList[i]);
+                if (i == 12)
+                    col0.Add(inputList[i]);
+                if (i == 13)
+                    col1.Add(inputList[i]);
+                if (i == 14)
+                    col2.Add(inputList[i]);
+                if (i == 15)
+                    col3.Add(inputList[i]);
+            }
+
+            // todo: delete later
+            Console.WriteLine("Columns before shifted:");
+            Console.Write("col0: ");
+            foreach (var a in col0)
+                Console.Write(a);
+            Console.WriteLine();
+            Console.Write("col1: ");
+            foreach (var b in col1)
+                Console.Write(b);
+            Console.WriteLine();
+            Console.Write("col2: ");
+            foreach (var c in col2)
+                Console.Write(c);
+            Console.WriteLine();
+            Console.Write("col3: ");
+            foreach (var d in col3)
+                Console.Write(d);
+
+            output = col0;
+            List<int> shiftedCol1 = ShiftOneDownColumnOne(col1);
+            output.AddRange(shiftedCol1);
+            List<int> shiftedCol2 = ShiftTwoDownColumnTwo(col2);
+            output.AddRange(shiftedCol2);
+            List<int> shiftedCol3 = ShiftThreeDownColumnThree(col3);
+            output.AddRange(shiftedCol3);
+
+            return output;
+        }
+
+        public List<int> ShiftOneDownColumnOne(List<int> inputList)
+        {
+            List<int> output = new List<int>();
+            for (int i = 0; i < inputList.Count; i++)
+            {
+                if (i == 0)
+                    output.Add(inputList[3]);
+                else
+                    output.Add(inputList[i - 1]);
+            }
+            return output;
+        }
+
+        public List<int> ShiftTwoDownColumnTwo(List<int> inputList)
+        {
+            List<int> output = new List<int>();
+            for (int i = 0; i < inputList.Count; i++)
+            {
+                if (i < 2)
+                    output.Add(inputList[i + 2]);
+                else
+                    output.Add(inputList[i - 2]);
+            }
+            return output;
+        }
+
+        public List<int> ShiftThreeDownColumnThree(List<int> inputList)
+        {
+            List<int> output = new List<int>();
+            for (int i = 0; i < inputList.Count; i++)
+            {
+                if (i == 0)
+                    output.Add(inputList[i + 3]);
+                else if (i == 1)
+                    output.Add(inputList[i + 1]);
+                else if (i == 2)
+                    output.Add(inputList[i - 1]);
+                else
+                    output.Add(inputList[i - 3]);
+            }
+            return output;
+        }
+
+        // Note: keeping it here just incase we need it
+        public List<int> PermutationTwo(List<int> inputList)
+        {
+            List<int> output = new List<int>();
+            output.Add(inputList[3]);   // 0>3
+            output.Add(inputList[15]);  // 1>15
+            output.Add(inputList[10]);  // 2>10
+            output.Add(inputList[12]);  // 3>12
+            output.Add(inputList[5]);   // 4>5
+            output.Add(inputList[13]);  // 5>13
+            output.Add(inputList[4]);   // 6>4
+            output.Add(inputList[8]);   // 7>8
+            output.Add(inputList[1]);   // 8>1
+            output.Add(inputList[11]);  // 9>11
+            output.Add(inputList[2]);   // 10>2
+            output.Add(inputList[0]);   // 11>0
+            output.Add(inputList[7]);   // 12>7
+            output.Add(inputList[14]);  // 13>14
+            output.Add(inputList[6]);   // 14>6
+            output.Add(inputList[9]);   // 15>9
+            return output;
+        }
     }
 }
